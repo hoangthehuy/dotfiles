dfdf7855846d75bb4e8d490485772a354a5ca174
Huy Hoang <huy.hoang@discordapp.com>
2025-01-22 23:13:50 +0000

    initial

---
+++ install.sh
@@ -0,0 +1,18 @@
+packages=(
+    'neofetch'  # TODO: switch off neofetch
+    'onefetch'
+)
+apt_repositories=(
+    'ppa:o2sh/onefetch'
+)
+
+for repository in ${apt_repositories[@]}; do
+    sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y $repository
+done
+
+sudo apt update -y
+sudo apt upgrade -y
+
+for package in ${packages[@]}; do
+    sudo apt install $package -y
+done
